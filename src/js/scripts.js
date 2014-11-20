2dc21a9c7b585962a9022b73cc6647522b2314c5
Escoffier Nicolas <nico.oxydesign@free.fr>
2014-11-20 02:22:57 +0100

    Mobile Carousel (Html/CSS/JS)

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,41 @@
 $(document).ready(function(){
-    $('.comparison').compare({
-    	selection:'.comparison-selection',
-    	results:'.comparison-results'
-    });
+	var selects = $('.comparison-selection select'),
+		results = $('.panel-toggle'),
+		mobileNav = $('.mobile-nav .panel-heading'),
+		selected = mobileNav.filter('.selected'),
+		eltIndex = selected.length ? mobileNav.index(selected) : 0,
+		mobileCarousel = $('.mobile-carousel .glyphicon'),
+		click = "onTap" in window ? 'tap' : 'click';
 
-    $(window).resize(function(){
-    	console.log('resize');
-    });
+	carouselUpdate();
+
+	mobileNav.add(mobileCarousel).on(click,function(){
+		var elt = $(this);
+		if(elt.hasClass('panel-heading')){
+			eltIndex = mobileNav.index(elt);
+		}else if(!elt.hasClass('inactive')){
+			elt.hasClass('glyphicon-chevron-right') ? eltIndex++ : eltIndex--;
+		}
+		carouselUpdate();
+	});
+
+	$('.comparison').compare({
+		selection:'.comparison-selection',
+		results:'.comparison-results'
+	});
+
+	function carouselUpdate(){
+		mobileNav.removeClass('selected');
+		mobileNav.eq(eltIndex).addClass('selected');
+
+		results.removeClass('active');
+		results.eq(eltIndex).addClass('active');
+
+		mobileCarousel.removeClass('inactive');
+		if(eltIndex === 0){
+			mobileCarousel.filter('.glyphicon-chevron-left').addClass('inactive');
+		}else if(eltIndex === mobileNav.length-1){
+			mobileCarousel.filter('.glyphicon-chevron-right').addClass('inactive');
+		}
+	}
 });
